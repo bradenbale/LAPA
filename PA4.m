622102bdc9004f917dd76b85593edb662a1507dc
bradenbale <59070001+bradenbale@users.noreply.github.com>
2021-02-03 17:44:59 -0500

    Add files via upload

---
+++ PA4.m
@@ -0,0 +1,41 @@
+% set(0, 'DefaultFigureWindowStyle', 'docked')
+
+nx = 50;
+ny = 50;
+max = 1000;
+V = zeros(nx, ny);
+
+SidesToZero = 1;
+
+for k = 1:max
+    for i = 1:nx
+        for j = 1:ny
+            if (i == 1) && (j == 1) 
+                V(i,j) = 1;
+            elseif i == 1 
+                V(i,j) = 1;
+            elseif i == nx
+                V(i,j) = SidesToZero;
+            elseif j == 1
+%                 V(i,j) = (V(i, j + 1) + V(i + 1, j) + V(i - 1, 1)) * 0.3333333333;
+                C(i, j) = 0;
+            elseif j == ny
+                V(i,j) = (V(i, j - 1) + V(i + 1, j) + V(i - 1, j)) * 0.3333333333;
+            else
+                V(i, j) = (V(i, j + 1) + V(i, j - 1) + V(i + 1,j) + V(i - 1, j)) * 0.25;
+            end    
+            
+        end
+        
+    end
+        
+    if mod(k,50) == 0
+        surf(V)
+        pause(0.1)
+    end
+end
+
+[Ex, Ey] = gradient(V);
+
+figure
+quiver(-Ey', -Ex', 1)
